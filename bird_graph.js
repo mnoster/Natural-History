1dd34f235d6dd0dcf065b06123ab7cecc65a48dd
Nicholas Porter <njporter10@gmail.com>
2016-09-25 20:06:23 -0700

    made user login system, fron and back end

--- bird_graph.js
+++ bird_graph.js
@@ -135,3 +135,34 @@ function initGraph(data, xCoordinate) {
     });
 
 }
+//this is for user login
+
+$(document).on('click', "#login-button", function () {
+    user_login();
+});
+function user_login() {
+    var username = $('#username').val();
+    console.log(username);
+    var password = $('#password').val();
+    console.log(password);
+
+    $.ajax({
+        url: 'login_handler.php',
+        method: 'POST',
+        data: {
+            username: username,
+            password: password
+        },
+        dataType: 'json',
+        success: function (response) {
+            // console.log("response is success: ", response);
+            if (response.success == true) {
+                window.location.replace('bird_chart.html');
+            }
+        },
+        error: function (response) {
+            // console.log("there was an error: ", response);
+            $('<div>').addClass("text-danger").text("Invalid code").appendTo('#error-message');
+        }
+    })
+}
